a7fcfa1de721403685973716355c382000f3aab1
Gorkem Korkmaz <gorkeminanckorkmaz@gmail.com>
2026-03-28 01:49:49 +0200

    feat: updated

---
+++ examples/complete/main.tf
@@ -0,0 +1,60 @@
+module "vm" {
+  source = "../../"
+
+  proxmox_endpoint  = var.proxmox_endpoint
+  proxmox_api_token = var.proxmox_api_token
+
+  # Common defaults
+  node_name        = "carbon"
+  clone_vm_id      = 9000
+  init_gateway     = "172.16.2.1"
+  init_dns_servers = ["8.8.8.8", "8.8.4.4"]
+  init_username    = "admin"
+  init_ssh_keys    = [var.ssh_public_key]
+
+  # CPU & Memory defaults
+  cpu_cores        = 4
+  memory_dedicated = 4096
+
+  # Disk defaults
+  disk_size         = 40
+  disk_datastore_id = "data"
+
+  vms = {
+    web-01 = {
+      name       = "web-01"
+      vm_id      = 200
+      ip_address = "172.16.2.10/24"
+      tags       = ["staging", "web"]
+
+      # Additional disk
+      disk_additional = [
+        {
+          size      = 20
+          interface = "virtio1"
+        }
+      ]
+
+      # Additional network device
+      network_additional = [
+        {
+          bridge = "vmbr1"
+        }
+      ]
+
+      # Additional IP configuration for second NIC
+      additional_ip_configs = [
+        {
+          address = "10.0.1.10/24"
+        }
+      ]
+    }
+
+    web-02 = {
+      name       = "web-02"
+      vm_id      = 201
+      ip_address = "172.16.2.11/24"
+      tags       = ["staging", "web"]
+    }
+  }
+}
---
+++ examples/complete/variables.tf
@@ -0,0 +1,15 @@
+variable "proxmox_endpoint" {
+  description = "Proxmox API endpoint"
+  type        = string
+}
+
+variable "proxmox_api_token" {
+  description = "Proxmox API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key content"
+  type        = string
+}
